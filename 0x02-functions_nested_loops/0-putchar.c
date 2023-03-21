08aa020a9a508180efd2e99c8d4be6c0259b603f
Jules Mugisha <mugishajules75@gmail.com>
2023-03-21 09:39:12 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main- entry point
- * return: 0
+ * main - Entry point
+ * Return:Always 0
  */
 
 int main(void)
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
