5468518ea56663a8ed822be85a4ca33d3d28c0d9
Jules Mugisha <mugishajules75@gmail.com>
2023-04-17 03:04:49 -0700

    sum added

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,4 @@
-i#ifndef _function_like_macro_h_
-#define _function_like_macro_h_
-
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-
-#endif /* _function_like_macro_h_ */
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif
