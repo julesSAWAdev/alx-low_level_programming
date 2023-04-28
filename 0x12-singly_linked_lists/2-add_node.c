59f10514ff9ff0d5327afbed8360b059c81f576a
Jules Mugisha <mugishajules75@gmail.com>
2023-04-28 04:22:43 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 int _strlen_recursion(char *s);
 
 /**
@@ -8,22 +7,26 @@ int _strlen_recursion(char *s);
  * @str: string to set in the new node.
  * Return: address of the new element, or NULL if it failed
  **/
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
+	
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 
 	new->str = strdup(str);
+	
 	if (!new->str)
 	{
 		free(new);
 		return (NULL);
 	}
+	
 	new->len = _strlen_recursion(new->str);
 	new->next = *head;
 	*head = new;
@@ -33,14 +36,19 @@ list_t *add_node(list_t **head, const char *str)
 
 /**
  * _strlen_recursion - returns the length of a string.
- * @s: string.
- * Return: length of @s.
+ * @s: string
+ * Return: length of string
  */
+
 int _strlen_recursion(char *s)
 {
 	if (*s == 0)
+	{
 		return (0);
+	}
 	else
+	{
 		return (1 + _strlen_recursion(s + 1));
+	}
 }
 
